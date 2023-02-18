905d4db49634263902ede8b5446b29e0d9655fa7
Simbala-Diabira <simbaladiabira7136@gmail.com>
2023-02-18 14:36:01 +0100

    Initialisation git

---
+++ Javascript/script.js
@@ -0,0 +1,13 @@
+// typing animation ou animation du nom de métier
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Développeur Web",
+    "Web Designer",
+    "Graphic Designer",
+    "YouTubeur",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
